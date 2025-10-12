0657738f6db84ff51783791abeb0c14397ea77db
itzisurii <itzIsurika08@gmail.com>
2025-10-12 09:51:47 +0530

    Implement Customer Dashboard UI

--- src/main/java/controller/CustomerManagementController.java
+++ src/main/java/controller/CustomerManagementController.java
@@ -1,4 +1,85 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerManagementController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colDOB;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableView<?> tblCustomerManagement;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCusID;
+
+    @FXML
+    private TextField txtDOB;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtProvince;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnAddActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
