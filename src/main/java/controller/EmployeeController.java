e7a7c0babe5c4a6542050d9f38155db443bfa5fa
itzisurii <itzIsurika08@gmail.com>
2025-11-25 23:48:48 +0530

    Add sample EmployeeManagementDTO data

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,18 +1,25 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.dto.EmployeeManagementDTO;
+import model.dto.ItemManagementDTO;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmployeeController implements Initializable {
 
-
+    ObservableList<EmployeeManagementDTO> employeeManagementDTOS = FXCollections.observableArrayList(
+            new EmployeeManagementDTO("E001", "john", "200567990", "2005-03-08", "Head", 120000),
+            new EmployeeManagementDTO("E002", "Ariana", "200577990", "2004-09-04", "Chief", 130000)
+    );
 
     @FXML
     private TableColumn<?, ?> colDOB;
