4a1f685d7e61122767a2d4304f8acfa33e84954f
itzisurii <itzIsurika08@gmail.com>
2025-11-25 23:40:16 +0530

    Created EmployeeController with UI bindings (columns, text fields) and placeholder action handlers

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,4 +1,75 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EmployeeController {
+
+    @FXML
+    private TableColumn<?, ?> colDOB;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colNIC;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPosition;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableView<?> tblItemManagement;
+
+    @FXML
+    private TextField txtDOB;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtNIC;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPosition;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnAddActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackToDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
