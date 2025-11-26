ebea19c7c8881b2c5c89a9a52cb3fbb0fb010d07
itzisurii <itzIsurika08@gmail.com>
2025-11-26 14:52:45 +0530

    Added controller logic to navigate from Employee Management to Dashboard

--- src/main/java/controller/EmployeeManagementController.java
+++ src/main/java/controller/EmployeeManagementController.java
@@ -4,15 +4,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.dto.EmployeeManagementDTO;
 import model.dto.ItemManagementDTO;
 import model.dto.SupplierManagementDTO;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -82,8 +86,14 @@ public class EmployeeManagementController implements Initializable {
     }
 
     @FXML
+    Stage stage =  new Stage();
     void btnBackToDashboardOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     @FXML
